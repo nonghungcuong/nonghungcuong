64a93d85e342bda78acae296bdc3441f9f7448f0
cuong ph <nonghungcuongxd@gmail.com>
2020-04-02 20:57:03 +0700

    thêm master

---
+++ resources/views/master.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <!--từ w3cshools.com
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    -->
+    <!--từ trang getbootstrap.com
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"  integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" defer integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" defer integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    -->
+    <link rel="stylesheet" href="../resources/views/bootstrap/css/bootstrap.min.css">
+    <script src="../resources/views/bootstrap/jquery/jquery-3.4.1.min.js"></script>
+    <script src="../resources/views/bootstrap/popper/popper.min.js" defer></script>
+    <script src="../resources/views/bootstrap/js/bootstrap.min.js" defer></script>
+    <link rel="stylesheet" href="../resources/views/css/css.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+        <button type="button" class="btn btn-warning myPopover"
+            data-toggle="popover"
+            data-placement="right" title="Right Popover"
+            data-content="Popover show on Right">Tooltip on right
+        </button>
+            
+            
+            <script>
+            $(function(){
+                $(".myPopover").popover();
+            });
+            </script>
+        </div>
+        <span class="badge badge-primary">Thông tin chính --- dammio.com</span>
+<span class="badge badge-secondary">Thông tin phụ</span>
+<span class="badge badge-success">Thao tác nào đó thành công</span>
+<span class="badge badge-danger">Nguy hiểm</span>
+<span class="badge badge-warning">Cảnh báo</span>
+<span class="badge badge-info">Thông tin cho nội dung</span>
+<span class="badge badge-light">Chữ màu sáng</span>
+<span class="badge badge-dark">Chữ màu tối</span>
+    </div>
+</body>
+</html>
